739f501c272ef74a581c91551c8b25ee2ae6e305
ComputerSayNo <jack14jack04@googlemail.com>
2023-05-11 18:10:08 +0100

    homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-neutral-700">
       <body className={inter.className}>
         <PrimaryHeader />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
+interface Blog {
+  title: string;
+  link: string;
+  excerpt: string;
+  date: string;
+}
+
 export default function Home() {
+  const blogs: Array<Blog> = [
+    {
+      title: "Training Biceps for Hypertrophy (Bodybuilding)",
+      link: "/blogs/training-biceps-for-hypertrophy",
+      date: "2023-05-11",
+      excerpt: `Biceps are one of the most popular muscle groups to train in the gym, 
+      and for good reason. Not only do well-developed biceps look impressive,
+      they also play a key role in many upper body movements, such as pulling
+      exercises like rows and chin-ups. But when it comes to training biceps
+      for hypertrophy (muscle growth), there are a few key principles to keep
+      in mind. In this blog, we'll go over those principles and provide some
+      tips for maximizing your biceps gains.`,
+    },
+  ];
+
   return (
-    <main className="container mx-auto pt-48">
-      <h1 className="text-5xl">
+    <main className="container mx-auto pt-28 mb-20">
+      <h1 className="text-5xl mb-11">
         Gym Head: Everything you need to know about the gym in 2023
       </h1>
+      <div className="flex text-lg">
+        {blogs.map((blog) => (
+          <a
+            href={blog.link}
+            key={blog.title}
+            className="flex flex-col w-96 border p-4 rounded-md border-neutral-700 hover:cursor-pointer "
+          >
+            <h2 className="font-medium text-2xl mb-3">{blog.title}</h2>
+            <p>{blog.excerpt}</p>
+            <span className="font-medium italic mt-2">{blog.date}</span>
+          </a>
+        ))}
+      </div>
     </main>
   );
 }
