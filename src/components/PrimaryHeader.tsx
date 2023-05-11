74ec5481fe1430ca660f1130cbf34d857d98cdf4
ComputerSayNo <jack14jack04@googlemail.com>
2023-05-11 19:38:54 +0100

    remove img

--- src/components/PrimaryHeader.tsx
+++ src/components/PrimaryHeader.tsx
@@ -5,7 +5,6 @@ export default function PrimaryHeader() {
     <div className="bg-sky-600 p-5 ">
       <nav className="container mx-auto flex text-white">
         <div className="me-7">
-          <img className="w-1" src="/vercel.svg" />
           <span className="font-semibold">Gym Head</span>
         </div>
         <Link href="/" className="hover:underline">
